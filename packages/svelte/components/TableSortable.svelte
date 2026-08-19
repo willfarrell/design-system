a6fe52b770ee67402df115629e1ca5a3def3439b
will Farrell <willfarrell@proton.me>
2026-08-19 11:08:21 -0600

    chore: version bump
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- packages/svelte/components/TableSortable.svelte
+++ packages/svelte/components/TableSortable.svelte
@@ -1,6 +1,6 @@
 <script>
 import webComponentUrl from "@willfarrell-ds/vanilla/components/ds-table-sortable.js?worker&url";
-import Div from "../elements/div.svelte";
+import Table from "./Table.svelte";
 
 const { children, ...props } = $props();
 </script>
@@ -8,9 +8,6 @@ const { children, ...props } = $props();
 <svelte:head>
   <link rel="modulepreload" href={webComponentUrl} />
 </svelte:head>
-<!-- div container needed to allow overflow-x on oversized tables -->
-<Div class="table-overflow" role="region" aria-label={props['aria-label'] ?? 'Data table'} tabindex="0">
-  <table is="ds-table-sortable" {...props}>
-    {@render children?.()}
-  </table>
-</Div>
+<Table is="ds-table-sortable" {...props}>
+  {@render children?.()}
+</Table>
